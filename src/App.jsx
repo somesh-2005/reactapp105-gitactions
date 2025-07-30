13709a6859e0ef2a7c4005f28d257c76f8b9062c
SOMESH <someshbiswal2308@gmail.com>
2025-07-30 10:17:17 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+function App(){
+  return (
+    <div className='container'>
+      <h1>Welcome to My React Project - S105</h1>
+      <p>This is a single language </p>
+    </div>
+  )
+}
